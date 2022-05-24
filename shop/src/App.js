dd2f77a0fb2552fb3bd4cd16e2d0cc4b83efa02e
hodokim <hodo323@naver.com>
2022-05-24 23:32:37 +0900

    import, export

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,21 +1,55 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
 
 function App() {
+
+  let [shoes, setShoes] = useState(data);
+
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeMaker</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
-      </Navbar>    </div>
+      </Navbar>   
+      
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map((x, i) => {
+              return (
+                <ProductInfo shoes={x} index={i} key={i}></ProductInfo>
+              )
+            })
+          }
+
+
+
+
+
+        </div>
+      </div>
+
+    </div>
   );
+}
+
+let ProductInfo = (props) => {
+  return (
+    <div className="col-md-4">
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.index + 1) + ".jpg"} alt={props.index} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
 }
 
 export default App;
