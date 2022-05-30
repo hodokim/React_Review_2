881a2aae8ef6e90b32c5fdcdb3ede1852301c341
hodokim <hodo323@naver.com>
2022-05-30 23:07:31 +0900

    Styled Component, useEffect()

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,17 +1,33 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from 'styled-components'
 
 let YellowBtn = styled.button`
-    background : yellow;
-    color : black;
+    background : ${props => props.bg};
+    color : ${ props => props.bg == 'blue' ? 'white' : 'black'};
     padding : 10px;
 `
 
 let DetailBox = (props) => {
-    let {id} = useParams();
+    let { id } = useParams();
+    let [eventFlag, setEvnFlag] = useState(true);
+    let [num, setNum] = useState('');
+
+    useEffect(()=>{
+        setTimeout(()=>{setEvnFlag(false)},2000)
+    });
+
+
     return (
         <div className="container">
-            <YellowBtn>button</YellowBtn>
+            {
+                eventFlag ? 
+                <div className="alert alert-warning">2초 이내 구매시 90% 할인!</div>
+                : null
+            }
+            <div>수량 <input onChange={(e)=>{
+                e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+            }}/></div>
             <div className="row">
                 <div className="col-md-6">
                     <img src={"https://codingapple1.github.io/shop/shoes" + (props.shoes[id].id+1) + ".jpg"} width="100%" alt={props.index} />
